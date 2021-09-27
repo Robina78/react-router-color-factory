df84f0168f8ca499c521354bb06ff05f501adebd
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-09-27 13:40:11 -0700

    react router pattern color factory

---
+++ src/NewColorForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./NewColorForm.css";
+
+const NewColorForm = ({addColor}) => {
+    const [form, setForm] = useState({name: "", hex:"#ffffff"});
+    const history = useHistory();
+
+    const handleChange = (evt) => {
+        evt.persist();
+        setForm(form => ({ ...form, [evt.target.name]: evt.target.value }));
+    };
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        addColor({ [form.name]: form.hex });
+        history.push("/colors");
+    };    
+    const {hex, name} = form;
+
+    return (
+        <div className="NewColor">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Color name</label>
+                    <input 
+                        name="name"
+                        id="name"
+                        placeholder="Enter color name"
+                        onChange={handleChange}
+                        value={name}
+                    />    
+                </div>
+                <div>
+                <label htmlFor="hex">Color value</label>
+                    <input 
+                        type="color"
+                        name="hex"
+                        id="hex"
+                        onChange={handleChange}
+                        value={hex}
+                    />    
+                </div>
+                <input type="Submit" value="Add this color" readOnly />
+            </form>
+        </div>
+    );
+};
+
+export default NewColorForm;
